45dd9dd468f046586bd469e8da3d746be07cabb4
Yogso92 <jordane.finfe@hotmail.com>
2019-07-01 14:02:11 +0200

    DataFilePostDeployment

--- ADO/PostDeployment.sql
+++ ADO/PostDeployment.sql
@@ -1,11 +1,15 @@
-﻿/*
-Modèle de script de post-déploiement							
---------------------------------------------------------------------------------------
- Ce fichier contient des instructions SQL qui seront ajoutées au script de compilation.		
- Utilisez la syntaxe SQLCMD pour inclure un fichier dans le script de post-déploiement.			
- Exemple :      :r .\monfichier.sql								
- Utilisez la syntaxe SQLCMD pour référencer une variable dans le script de post-déploiement.		
- Exemple :      :setvar TableName MyTable							
-               SELECT * FROM [$(TableName)]					
---------------------------------------------------------------------------------------
-*/
+﻿SET IDENTITY_INSERT Employee ON;
+
+INSERT INTO Employee (Id,FirstName,LastName,Email, BirthDate, ResponsableID)
+VALUES
+(1,	'Ken','Sánchez','ken0@adventure-works.com','1969-01-29',NULL),
+(16,'David','Bradley','david0@adventure-works.com','1975-03-19',273),
+(23,'Mary','Gibson','mary0@adventure-works.com','1962-09-13',16),
+(273,'Brian','Welcker','brian3@adventure-works.com','1977-06-06',1),
+(274,'Stephen','Jiang','stephen0@adventure-works.com','1951-10-17',273),
+(275,'Michael','Blythe','michael9@adventure-works.com','1968-12-25',274),
+(276,'Linda','Mitchell','linda3@adventure-works.com','1980-02-27',274),
+(285,'Syed','Abbas','syed0@adventure-works.com','1975-01-11',273),
+(286,'Lynn','Tsoflias','lynn0@adventure-works.com','1977-02-14',285);
+
+SET IDENTITY_INSERT Employee OFF;
